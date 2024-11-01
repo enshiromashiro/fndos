d42c09b7d4b948747c637dc38163adf4d124a8ec
enshiromashiro <cerasus45@gmail.com>
2024-11-02 02:37:24 +0900

    Modify for this project
    - Add a LICENSE file
    - Write README.md
    - Introduce a mock UI
    - Remove unused files
    - Use biome instead of eslint

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const setup = () => {
+	const root = document.getElementById("root");
+	if (!root) {
+		console.error("#root is not found.");
+		return;
+	}
+
+	createRoot(root).render(
+		<StrictMode>
+			<App />
+		</StrictMode>,
+	);
+};
+setup();
