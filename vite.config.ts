3bd4563a0f12538b05727d501248761ea247a799
enshiromashiro <cerasus45@gmail.com>
2024-11-08 00:19:53 +0900

    Fix sysinfo

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-
-type System = {
-  version: string,
-  commit: string,
-}
-const system: System = {
-  version: "NULL",
-  commit: "NULL",
-};
-
-import("./system-info.json").then((sys) => {
-  system.version = sys.version;
-  system.commit = sys.commit;
-}).catch(() => {});
+import system from "./system-info.json" // run `yarn prebuild`
 
 // https://vite.dev/config/
 export default defineConfig({
